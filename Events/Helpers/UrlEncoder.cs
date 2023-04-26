1b4b1e305281c40b039b28e14baf3c19b79d6c1f
Vladyslav Vyshnivskyi <vladyslav.vyshnivskyi@avanquest.com>
2023-04-26 22:51:49 +0300

    Add Url Encoder

---
+++ Events/Helpers/UrlEncoder.cs
@@ -0,0 +1,34 @@
+﻿using System.Web;
+
+namespace Events.UI.Helpers
+{
+    public static class UrlEncoder
+    {
+        public static string EncodeQueryKeys(string? url)
+        {
+            if (url == null)
+            {
+                return "";
+            }
+
+            var queryString = new Uri(url).Query;
+            var nameValueCollection = HttpUtility.ParseQueryString(queryString);
+            var queryList = new List<KeyValuePair<string, string>>();
+
+            foreach (string key in nameValueCollection.Keys)
+            {
+                //string encodedKey = HttpUtility.UrlEncode(key);
+                string encodedKey = Uri.EscapeDataString(key);
+                string value = nameValueCollection[key] ?? "";
+                //string encodedValue = HttpUtility.UrlEncode(value);
+                string encodedValue = Uri.EscapeDataString(value);
+                queryList.Add(new KeyValuePair<string, string>(encodedKey, encodedValue));
+            }
+
+            string encodedQuery = string.Join("&", queryList.Select(pair => $"{pair.Key}={pair.Value}"));
+            string encodedUrl = $"{url.Split('?')[0]}?{encodedQuery}";
+
+            return encodedUrl;
+        }
+    }
+}
